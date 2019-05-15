fb29e2aa6ee4c67ecf44ddc911edc56811cf0597
Callum Bryant <callum.l.bryant@gmail.com>
2019-05-15 21:28:59 +1000

    Updates

---
+++ precache-manifest.536bd8a70e5ffa1b633ee0ccd8d89925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23df2b241b35237d5dd1925e8379feb2",
+    "url": "/artbuffet/index.html"
+  },
+  {
+    "revision": "53236b538fce0b117b60",
+    "url": "/artbuffet/static/css/main.b838ec53.chunk.css"
+  },
+  {
+    "revision": "e886b92d65b53483fad1",
+    "url": "/artbuffet/static/js/2.d82f303b.chunk.js"
+  },
+  {
+    "revision": "53236b538fce0b117b60",
+    "url": "/artbuffet/static/js/main.ba6a72e3.chunk.js"
+  },
+  {
+    "revision": "76f8f7a99de64725317e",
+    "url": "/artbuffet/static/js/runtime~main.a776686e.js"
+  }
+]);

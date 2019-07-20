a158643e58043463c5378bd90a9486ded6ba99ab
Callum Bryant <callum.l.bryant@gmail.com>
2019-07-20 13:03:14 +1000

    start adding some ui for filtering

--- src/GalleryApp.tsx
+++ src/GalleryApp.tsx
@@ -9,6 +9,7 @@ import {GalleryGrid} from "./components/GalleryGrid";
 const GalleryApp: React.FC = () => {
   return (
     <div className="App">
+        <div id="options-bar"><p>This is where selection options will appear</p></div>
       <GalleryGrid></GalleryGrid>
     </div>
   );
